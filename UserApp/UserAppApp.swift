4b84e5acb67c0abf115466744071d883f901fd5b
rhenz <salvador.rhenz@gmail.com>
2023-05-23 14:25:39 +0800

    Initial Commit

---
+++ UserApp/UserAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserAppApp.swift
+//  UserApp
+//
+//  Created by John Salvador on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct UserAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
